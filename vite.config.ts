699e3ef61df641dd791b22fd76c1526e377152a2
komgen <komuro@corkagency.com>
2025-05-12 11:31:23 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,14 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['google-spreadsheet']
+    }
+  }
+})
