4527cac1ee14e9f72f5da915bf7e852a46bf9045
komgen <komuro@corkagency.com>
2025-05-17 15:19:37 +0900

    Header.tsx を更新

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,13 @@
-
-import React from "react";
+import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="bg-manga-primary text-white py-4">
-      <div className="container">
-        <h1 className="text-xl font-bold">漫画商品サンプル送付システム</h1>
+    <header className="bg-manga-primary text-white">
+      <div className="container mx-auto py-4">
+        {/* サイト全体のタイトルを変更 */}
+        <h1 className="text-xl font-bold">
+          UKSSS-01（宇宙兄弟サンプル送付システム）
+        </h1>
       </div>
     </header>
   );
